08509a222a02e369f0ee06126df0c16e61084031
Josh <funkygamer20@outlook.com>
2021-06-12 18:48:39 +0200

    tried to change the camera to follow each player, failed. Added cinemachine camera for smoother movement

--- GMTK 2021 Game/Assets/Scripts/Camera_script.cs
+++ GMTK 2021 Game/Assets/Scripts/Camera_script.cs
@@ -1,21 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class Camera_script : MonoBehaviour
 {
-    public Transform currentPlayer;
+   
+   
+    public CinemachineVirtualCamera leftcam;
+    public CinemachineVirtualCamera rightcam;
+    private bool canpressQ;
+    public float stopQtime;
+
+    private bool left = true;
     // Start is called before the first frame update
     void Start()
     {
         
+        
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        Vector3 position = transform.position;
-        position.y = (currentPlayer.position).y;
-        transform.position = position;
+        if (left)
+        {
+            leftcam.Priority = 1;
+            rightcam.Priority = 0;
+        }
+        else
+        {
+            leftcam.Priority = 0;
+            rightcam.Priority = 1;
+        }
+        
+        if (Input.GetKeyDown(KeyCode.Q) && canpressQ)
+        {
+            left = !left;
+            canpressQ = false;
+            StartCoroutine(Reset());
+        }
+       
+    }
+
+    private IEnumerator Reset()
+    {
+        yield return new WaitForSeconds(stopQtime);
+        canpressQ = true;
     }
 }
