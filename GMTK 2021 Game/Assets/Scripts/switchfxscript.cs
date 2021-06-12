cb2c3467b4495ebb303a439744cdcbcad1baa2ae
oliverwooding <oliverwooding@icloud.com>
2021-06-12 17:38:24 +0200

    conflicts resolved, though it seems to have broken the camera shake?

--- GMTK 2021 Game/Assets/Scripts/switchfxscript.cs
+++ GMTK 2021 Game/Assets/Scripts/switchfxscript.cs
@@ -7,6 +7,8 @@ public class switchfxscript : MonoBehaviour
     public AudioClip switchClip;
     public Transform cameraTransform;
     public shake_camera shake_Camera;
+    private bool canpressQ;
+    public float stopQtime;
 
     void Start()
     {
@@ -15,10 +17,19 @@ public class switchfxscript : MonoBehaviour
 
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Q))
-        {
-            AudioSource.PlayClipAtPoint(switchClip, cameraTransform.position);
-            shake_Camera.CamShake();
-        }
+            if (Input.GetKeyDown(KeyCode.Q) && canpressQ)
+            {
+                AudioSource.PlayClipAtPoint(switchClip, cameraTransform.position);
+                shake_Camera.CamShake();
+                canpressQ = false;
+                StartCoroutine(Reset());            
+            }
+        
+    }
+
+    private IEnumerator Reset()
+    {
+        yield return new WaitForSeconds(stopQtime);
+        canpressQ = true;
     }
 }
